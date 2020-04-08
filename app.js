018c19c47b5f761f0b5132791a6ab94a8ee70f96
dianmjs <dianamjs69@gmail.com>
2020-04-08 11:14:32 -0500

    commit solucion

--- app.js
+++ app.js
@@ -1,36 +1,8 @@
 const express = require("express");
 const app = express();
 
-let usuario = {
-  nombre: "",
-};
-let respuesta = {
-  error: false,
-  codigo: 200,
-  mensaje: "",
-};
-
-app.get("/usuario", function (req, res) {
-  respuesta = {
-    error: false,
-    codigo: 200,
-    mensaje: "",
-  };
-  if (usuario.nombre === "") {
-    respuesta = {
-      error: true,
-      codigo: 501,
-      mensaje: "Hola desconocido!",
-    };
-  } else {
-    respuesta = {
-      error: false,
-      codigo: 200,
-      mensaje: "respuesta del usuario",
-      respuesta: "Hola" + usuario + "!",
-    };
-  }
-  res.send("<h1>respuesta</h1>");
+app.get("/", (req, res) => {
+  res.send("Hola " + req.query.name + "!");
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
