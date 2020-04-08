1c8b7e981e40a27209d21ebb5ac0eb0933f0c2ff
dianmjs <dianamjs69@gmail.com>
2020-04-08 11:24:21 -0500

    otro1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hola " + req.query.name + "!..");
+  res.send(<h1>"Hola " + req.query.name + "!"</h1>);
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
